bb4604a66893c5caefa6ca4cf6f9cafb63065ca7
Noufal Ibrahim <noufal@nibrahim.net.in>
2011-09-23 19:58:19 +0530

    Update views properly when retrieving case lists

--- openlibrary/core/support.py
+++ openlibrary/core/support.py
@@ -56,25 +56,25 @@ class Support(object):
         c = Case.load(self.db, caseid)
         return c
         
-    def get_all_cases(self, typ = "all", summarise = False, sortby = "lastmodified", desc = "true", user = False):
+    def get_all_cases(self, typ = "all", summarise = False, sortby = "lastmodified", desc = "true", user = False, staleok = False):
         "Return all the cases in the system"
+        args = {}
         if summarise:
             d = defaultdict(lambda: 0)
             if user:
-                print "Filtered"
-                v = ViewDefinition("cases", "assignee", "", group_level = 2, stale = "ok", 
-                                   startkey = [user, "closed"], endkey = [user, {}])
+                args = dict(startkey = [user, "closed"], 
+                            endkey = [user, {}])
             else:
-                print "Open"
-                v = ViewDefinition("cases", "assignee", "", group_level = 2, stale = "ok", 
-                                   startkey = [None, "closed"], endkey = [None,{}])
+                args = dict(startkey = [None, "closed"], 
+                            endkey = [None,{}])
+            if staleok:
+                args.update(stale = "ok")
+            v = ViewDefinition("cases", "assignee", "", group_level = 2, **args)
             for i in v(self.db):
-                print "%s    %s    %s"%(i, i.key[1], i.value)
                 d[i.key[1]] += i.value
-            print d
             return d
         else:
-            return Case.all(self.db, typ, sortby, desc)
+            return Case.all(self.db, typ, sortby, desc, staleok)
             
             
 class Case(Document):
@@ -180,7 +180,7 @@ class Case(Document):
         return ret
 
     @classmethod
-    def all(cls, db, typ="all", sort = "status", desc = "false"):
+    def all(cls, db, typ="all", sort = "status", desc = "false", staleok = False):
         view = {"created"      : "cases/sort-created",
                 "caseid"       : "cases/sort-caseid",
                 "assigned"     : "cases/sort-assignee",
@@ -194,10 +194,11 @@ class Case(Document):
                          descending = desc)
         else:
             extra = dict(descending = desc)
-
+        if staleok:
+            extra["stale"] = "ok"
         if typ == "all":
             view = view.replace("-","-all-") 
-            result = cls.view(db, view, include_docs = True, stale = "ok", **extra)
+            result = cls.view(db, view, include_docs = True, **extra)
             return result.rows
         elif typ == "new":
             startkey, endkey = (["new"], ["replied"])
@@ -214,7 +215,8 @@ class Case(Document):
             extra['startkey'] = startkey
         if endkey:
             extra['endkey'] = endkey
-        result = cls.view(db, view, include_docs = True, stale = "ok", **extra)
+
+        result = cls.view(db, view, include_docs = True, **extra)
         return result.rows
         
     def __eq__(self, second):
